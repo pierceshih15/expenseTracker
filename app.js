510853ce8296ef7849d7a1783b44e576e6df0db4
pierce <pierceshih@gmail.com>
2019-04-29 12:30:33 +0800

    Add Mongoose connection and create Record and User Model.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const Record = require('./models/record');
+const User = require('./models/user');
+
+// 建立 DB 連線
+mongoose.connect('mongodb://localhost/expense-tracker', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('Oh no, mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('Okay, mongodb is connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Expense Tracker Project Init.')
---
+++ models/record.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema);
