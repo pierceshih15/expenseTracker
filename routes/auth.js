3e4ad09e395874f827f177a75c5ba5d8bc9cf154
pierce <pierceshih@gmail.com>
2019-04-30 16:08:33 +0800

    Add Procfile.

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express();
 const passport = require('passport');
 
-router.get('/facebook',
+// 一個是使用者要透過 Facebook 登入時的網址 /facebook
+router.get(
+  '/facebook',
   passport.authenticate('facebook', {
-    scope: [
-      'email',
-      'public_profile',
-    ]
+    scope: ['email', 'public_profile'] // Ask for user info
   })
 );
 
-router.get('/facebook/callback',
+// 一個則是當我們取得「授權許可」後 redirect 的網址 /facebook/callback
+router.get(
+  '/facebook/callback',
   passport.authenticate('facebook', {
     successRedirect: '/',
     failureRedirect: '/users/login',
