d9497f01c7c65b95aad0cbbc401882506282ff1d
pierce <pierceshih@gmail.com>
2019-04-30 11:30:05 +0800

    Create filter function by month and category.

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express();
 const Record = require('../models/record');
 const monthList = require('../public/data/months.json').results;
-const mongoose = require('mongoose');
+const categoryList = require('../public/data/categories.json').results;
 
 router.get('/', (req, res) => {
-  Record.find((err, records) => {
+  // 宣告變數作為篩選條件
+  const filterMonth = req.query.filterMonth || '';
+  const filterMonthRegExp = new RegExp("2019-" + filterMonth, "i");
+  const filterCategory = req.query.filterCategory || '';
+  const filterCategoryRegExp = new RegExp(filterCategory, "i");
+  Record.find({
+    date: {
+      $regex: filterMonthRegExp
+    },
+    category: {
+      $regex: filterCategoryRegExp
+    }
+  }).sort().exec((err, records) => {
     if (err) return console.error(err);
     // 計算總和
     let totalAmount = 0;
@@ -13,9 +25,12 @@ router.get('/', (req, res) => {
       totalAmount = records.map(record => parseInt(record.amount)).reduce((a, b) => a + b);
     }
     return res.render('index', {
-      records: records,
-      monthList: monthList,
-      totalAmount: totalAmount,
+      records,
+      monthList,
+      categoryList,
+      totalAmount,
+      filterMonth,
+      filterCategory
     });
   })
 })
